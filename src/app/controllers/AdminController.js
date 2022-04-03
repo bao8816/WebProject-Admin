0210c1687a3701897c02399107dd18fbdcd08036
PBAO <nile1087@gmail.com>
2022-04-03 18:35:13 +0700

    Database

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,33 +1,36 @@
+const products = require('../models/Product');
+const { multipleMongooseToObject } = require('../../util/mongoose');
+
 class AdminController {
     //GET "/"
     home(req, res) {
         res.render('home', {layout: 'home-layout'})
-    }
+    };
 
     //GET "/upload"
     upload(req, res) {
         res.render('upload', {layout: 'upload-layout'})
-    }
+    };
 
     //GET "/dashboard"
     dashboard(req, res) {
         res.render('dashboard', {layout: 'dashboard-layout'})
-    }
+    };
 
     //GET "/profile"
     profile(req, res) {
         res.render('profile', {layout: 'profile-layout'})
-    }
+    };
 
     //GET "/login"
     login(req, res) {
         res.render('login', {layout: 'iden-layout'})
-    }
+    };
 
     //GET "/signup"
     signup(req, res) {
         res.render('signup', {layout: 'iden-layout'})
-    }
-}
+    };
+};
 
 module.exports = new AdminController;
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const adminController = require('../app/controllers/AdminController')
+const express = require('express');
+const router = express.Router();
+const adminController = require('../app/controllers/AdminController');
 
-router.use('/upload', adminController.upload)
-router.use('/dashboard', adminController.dashboard)
-router.use('/profile', adminController.profile)
-router.use('/login', adminController.login)
-router.use('/signup', adminController.signup)
-router.use('/', adminController.home)
+router.get('/upload', adminController.upload);
+router.get('/dashboard', adminController.dashboard);
+router.get('/profile', adminController.profile);
+router.get('/login', adminController.login);
+router.get('/signup', adminController.signup);
+router.get('/', adminController.home);
 
-module.exports = router
+module.exports = router;
