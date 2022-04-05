05275322531effa0dde3ffa25383c9e81c9db6a0
TrungTin1701 <63804279+TrungTin1701@users.noreply.github.com>
2022-04-05 10:08:49 +0700

    update

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,6 +7,7 @@ router.get('/dashboard', adminController.dashboard);
 router.get('/profile', adminController.profile);
 router.get('/login', adminController.login);
 router.get('/signup', adminController.signup);
+router.get('/dashboard-customer',adminController.edit);
 router.get('/', adminController.home);
 
 module.exports = router;
