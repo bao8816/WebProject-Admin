745e2a1b9b0840f80871d72430713622157f42e5
PBAO <nile1087@gmail.com>
2022-04-12 17:57:58 +0700

    Fix login

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const adminController = require('../app/controllers/AdminController');
 
 router.get('/profile', adminController.profile);
-// router.get('/login', adminController.login);
-// router.get('/signup', adminController.signup);
-// router.post('/signup', adminController.signupr);
-// router.post('/login', adminController.loginr);
 router.get('/dashboard-customer',adminController.show_customer);
 router.get('/', adminController.dashboard);
 router.get('/oder',adminController.oder_list)
