32948a71817e4a7a3d0363a05833e1f403df6eea
PBAO <nile1087@gmail.com>
2022-04-26 21:59:11 +0700

    fix bugs for heroku

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI;
 const APP_ID = process.env.APP_ID;
 const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT;
 
 module.exports = {
     MONGO_URI,
     APP_ID,
     API_KEY,
+    PORT,
 };
